3723e2028b672facfcc40567c44608622dab8bd5
Megnijessie <megnijessie548@gmail.com>
2023-07-18 11:17:22 +0100

    Version 2 : Commit N 7 active navbar nav-link when is the current selected

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add active class to the current button (highlight it)
+var header = document.getElementById("Navbar");
+var navlinks = header.getElementsByClassName("nav-link");
+for (var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
