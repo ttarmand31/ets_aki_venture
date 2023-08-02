f797fb9685d71409bcdc3a776c954daef43e929a
ttarmand30 <ttarmand30@gmail.com>
2023-08-02 15:48:19 +0100

    Version 2 : Commit N 8 manage section realisation, section contact, section localisation, add modal action to zoom image after click

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ for (var i = 0; i < navlinks.length; i++) {
         this.className += " active";
     });
 }
+function hello(){
+    alert("Mesage envoyer avec succes");
+}
+
+document.addEventListener("click",function (e) {
+    if (e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'),)
+        myModal.show();
+    }
+})
