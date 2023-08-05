a771b9f5596d1ce4b5771cf151f83103247d060f
ttarmand30 <ttarmand30@gmail.com>
2023-08-05 11:34:18 +0100

    Version 4  : Commit N°12 add section contact,( hour, email form and address information); add user counter and moment fort section

--- script.js
+++ script.js
@@ -22,3 +22,60 @@ document.addEventListener("click",function (e) {
         myModal.show();
     }
 })
+
+function display_c(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_day(num){
+    if (num===1){
+        return "Lundi, ";
+    }
+    else if (num===2){
+        return "Mardi, ";
+    }
+    else if (num===3){
+        return "Mercredi, ";
+    }
+    else if (num===4){
+        return "Jeudi, ";
+    }
+    else if (num===5){
+        return "Vendredi, ";
+    }
+    else if (num===6){
+        return "Samedi, ";
+    }
+    else if (num===7){
+        return "Dimanche, ";
+    }
+}
+
+function display_status(num){
+    if(num>=8 && num<=18){
+        return " Nous sommes Ouvert"
+    }else{
+        return " Nous sommes fermes"
+    }
+}
+
+function display_color(num) {
+    if (num >= 8 && num <= 18) {
+        return "#0af51d"
+    } else {
+        return "#ff0000"
+    }
+}
+
+function display_ct() {
+    var x = new Date()
+    var x1=x.getMonth() + 1+ "-" + x.getDate() + "-" + x.getFullYear();
+    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+    x2 = display_day(x.getDay())+""+x1
+    x3 = " "+display_status(x.getHours( ))
+    document.getElementById('ct').innerHTML = x2;
+    document.getElementById('status').innerHTML = x3;
+    document.getElementById('status').style.color = display_color(x.getHours( ))
+    display_c();
+}
